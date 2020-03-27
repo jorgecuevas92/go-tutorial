d2180a30f8ab745f62f076fea99184fc356a37e3
Jorge Cuevas <jorge.cuevas.92@gmail.com>
2020-03-27 09:49:08 -0600

    Begining of booleans

--- src/github.com/jorgecuevas92/go-tutorial/Main.go
+++ src/github.com/jorgecuevas92/go-tutorial/Main.go
@@ -2,12 +2,11 @@ package main
 
 import (
 	"fmt"
-	"strconv"
 )
 
 func main() {
-	var i int = 42
-	var j string
-	j = strconv.Itoa(i)
-	fmt.Printf("%v, %T\n", j, j)
+	var n bool
+	m := 2 == 2
+	fmt.Printf("%v, %T\n", n, n)
+	fmt.Printf("%v, %T\n", m, m)
 }
